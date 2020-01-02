b239aa38f43c16cc49b3f6c695ca715878fab838
Maykon Menezes <maykon.meneses@gmail.com>
2020-01-02 15:57:13 -0300

    uploading the base project

---
+++ spec/support/number_helper_spec.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+
+describe 'NumberHelper' do
+
+  include NumberHelper
+
+  describe '#number_to_currency' do
+
+    context 'using default values' do
+
+      it "correctly formats an integer" do
+        expect(number_to_currency(2000)).to eq('$2,000.00')
+      end
+
+      it "correctly formats a float" do
+        expect(number_to_currency(2000.00)).to eq('$2,000.00')
+      end
+
+      it "correctly formats a string" do
+        expect(number_to_currency('2000')).to eq('$2,000.00')
+      end
+
+      it "uses delimiters for very large numbers" do
+        expect(number_to_currency(200000000000)).to eq('$200,000,000,000.00')
+      end
+
+      it "does not have delimiters for small numbers" do
+        expect(number_to_currency(200)).to eq('$200.00')
+      end
+
+    end
+
+    context 'using custom options' do
+
+      it 'allows changing the :unit' do
+        expect(number_to_currency(2000, :unit => "€")).to eq('€2,000.00')
+      end
+
+      it 'allows changing the :precision' do
+        expect(number_to_currency(2000, :precision => 0)).to eq('$2000')
+      end
+
+      it 'omits the separator if :precision is 0' do
+        
+
+      it 'allows changing the :delimiter'
+
+      it 'allows changing the :separator'
+
+      it 'correctly formats using multiple options'
+
+    end
+
+  end
+
+end
